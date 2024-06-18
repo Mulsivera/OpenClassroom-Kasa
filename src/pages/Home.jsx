41fa0573b2389e959c88a86e1f63bd57efc4391b
Adrien Da Silva Ferreira <adrien@utopios.solutions>
2024-06-18 13:53:43 +0200

    Corrections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import Banner from '../components/Banner';
 
 function Home() {
     return (
-        <Banner bannerbackground={"BackgroundHome"} bannertext={"Chez vous, partout et ailleurs"} />
+        <div>
+            <Banner bannerbackground={"BackgroundHome"} bannertext={"Chez vous, partout et ailleurs"} />
+        </div>
     )
 }
 
